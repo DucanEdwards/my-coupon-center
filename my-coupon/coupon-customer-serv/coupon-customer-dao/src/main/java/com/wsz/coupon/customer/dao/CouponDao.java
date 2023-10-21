3f0a5ecdc82f7147acaa61a38f1ec047871092d8
DucanEdwards <305185235@qq.com>
2023-10-22 00:38:02 +0800

    搭建coupon-customer-impl

--- my-coupon/coupon-customer-serv/coupon-customer-dao/src/main/java/com/wsz/coupon/customer/dao/CouponDao.java
+++ my-coupon/coupon-customer-serv/coupon-customer-dao/src/main/java/com/wsz/coupon/customer/dao/CouponDao.java
@@ -1,8 +1,9 @@
 package com.wsz.coupon.customer.dao;
 
+import com.wsz.coupon.customer.dao.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface CouponDao extends JpaRepository<CouponDao,Long> {
+public interface CouponDao extends JpaRepository<Coupon,Long> {
     long countByUserIdAndTemplateId(Long userId,Long templateId);
 }
